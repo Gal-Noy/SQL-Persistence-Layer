eaa697275a86dc971b4f9c364c6aa2bbcd36d1db
Yairderry <derry.yair@gmail.com>
2023-01-16 17:34:41 +0200

    fixed quantity updating

--- action.py
+++ action.py
@@ -11,7 +11,7 @@ def main(args: list[str]):
             curr_quantity = int(repo.execute_command(f"SELECT quantity FROM products where id = {split_line[0]}")[0][0])
             quantity_diff = int(split_line[1])
             if quantity_diff > 0 or (quantity_diff < 0 and curr_quantity >= quantity_diff):
-                repo.execute_command(f"UPDATE products SET quantity = {curr_quantity + quantity_diff}")
+                repo.execute_command(f"UPDATE products SET quantity = {curr_quantity + quantity_diff} WHERE id = {split_line[0]}")
                 repo.tables_daos['Activities'].insert(Activitie(*split_line))
 
 
